45cdfb1ff5b18d8da9b0f12214d1ba23abb7a890
martinxtim <44854028+martinxtim@users.noreply.github.com>
2019-01-24 18:13:16 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,10 @@
-
+$(document).scroll(function() {
+  var scrollPosition = $(window).scrollTop();
+  
+  if (scrollPosition > 155) {
+    $('#navbar').addClass('sticky');
+  }
+  else{
+    $('#navbar').removeClass('sticky');
+   }	
+});
